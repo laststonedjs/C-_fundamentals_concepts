a1b19335d8eade84d187c974d796a069bae5933e
Filip <filipsarovic428@gmail.com>
2023-04-21 22:16:07 +0200

    ran the first unit test

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,7 +8,7 @@ namespace GradeBook
 		static void Main(string[] args)
 		{
 
-			var book = new Book("Scott's Grade Book");
+			var book = new Book("Filip's Grade Book");
 			book.AddGrade(89.1);
 			book.AddGrade(90.5);
 			book.AddGrade(77.5);
