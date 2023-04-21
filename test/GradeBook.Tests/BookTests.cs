ec00587cde750720e4a85836541f51a01fa589b9
Filip <filipsarovic428@gmail.com>
2023-04-21 18:16:43 +0200

    first commit

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace GradeBook.Tests;
+
+public class BookTests
+{
+	[Fact]
+	public void Test1()
+	{
+		// arrange
+		var book = new Book("");
+
+		// act
+
+
+		// assert
+
+	}
+}
